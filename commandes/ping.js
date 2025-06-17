0b2bdff1fc48bae589f7b493609a61b232d7f1a4
Charles ke <167456480+Charleskenya1@users.noreply.github.com>
2025-06-18 02:14:51 +0300

    Update ping.js

--- commandes/ping.js
+++ commandes/ping.js
@@ -1,9 +1,9 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const { zokou } = require("../framework/zokou");
-zokou({ nomCom: "test", reaction: "⚡", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
+zokou({ nomCom: "test1", reaction: "⚡", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
     console.log("Commande saisie !!!s");
-    let z = '*🟢CHARLESKE-VMD 𝒊𝒔 𝒐𝒏𝒍𝒊𝒏𝒆🟢* 💡 \n\n ' + "𝑻𝒉𝒆 𝒃𝒐𝒕 𝒊𝒔 𝒄𝒖𝒓𝒓𝒆𝒏𝒕𝒍𝒚 𝒘𝒐𝒓𝒌𝒊𝒏𝒈 𝒐𝒏 𝒂 𝒈𝒐𝒐𝒅 𝒔𝒑𝒆𝒆𝒅😉👍";
+    let z = '*🟢CHARLESKE-XMD 𝒊𝒔 𝒐𝒏𝒍𝒊𝒏𝒆🟢* 💡 \n\n ' + "𝑻𝒉𝒆 𝒃𝒐𝒕 𝒊𝒔 𝒄𝒖𝒓𝒓𝒆𝒏𝒕𝒍𝒚 𝒘𝒐𝒓𝒌𝒊𝒏𝒈 𝒐𝒏 𝒂 𝒈𝒐𝒐𝒅 𝒔𝒑𝒆𝒆𝒅😉👍";
     let d = '                                                                           𝑯𝒆𝒂𝒍𝒕𝒉 𝒔𝒕𝒂𝒕𝒖𝒔✨';
     let varmess = z + d;
     var mp4 = 'https://files.catbox.moe/wmf0s3.mp4';
